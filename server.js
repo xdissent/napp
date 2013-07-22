3acedf137444a12e11cfd1a7b2e5e1aa0a932582
Greg Thornton <xdissent@me.com>
2013-07-21 20:58:41 -0500

    HTTP 1.1
    Add content length header

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express'),
     port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('nappy');
+  var body = 'napp\n';
+  res.header('Content-Length', body.length.toString());
+  res.send(body);
 });
 
 app.listen(port)
